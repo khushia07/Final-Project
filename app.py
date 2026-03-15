5345ce5d73b49d935a36f86b145fd6358a654431
Khushi Agrawal <agrawkhu@uw.edu>
2026-03-14 23:06:13 -0700

    everything

---
+++ app.py
@@ -0,0 +1,107 @@
+from flask import Flask, redirect, request, session, render_template
+from authlib.integrations.requests_client import OAuth2Session
+from keys import client_id, client_secret
+from functions import (
+    geocode_location,
+    days_ahead,
+    get_weather,
+    map_weather_to_genre,
+    get_spotify_tracks
+)
+
+app = Flask(__name__)
+
+app.secret_key = "abcdefghijklmnopqrstuvxyz"
+
+
+# this is where the user sees the form
+@app.route("/", methods =["GET", "POST"])
+
+def index():
+    if request.method == "POST":
+        session["date"] = request.form["date"]
+        session["location"] = request.form["location"]
+        session["length"] = request.form["length"]
+
+        return redirect("/login")
+    
+    return render_template("index.html")
+
+# login route
+@app.route("/login")
+def login():
+    scope = "user-read-private user-read-email user-top-read"
+
+    client = OAuth2Session(
+        client_id,
+        client_secret,
+        scope=scope,
+        redirect_uri="http://127.0.0.1:5000/callback"
+    )
+
+    authorization_endpoint = "https://accounts.spotify.com/authorize"
+
+    uri, state = client.create_authorization_url(authorization_endpoint, show_dialog=True)
+
+    session["oauth_state"] = state
+    return redirect(uri)
+
+
+# callback route
+@app.route("/callback")
+def callback():
+
+    scope = "user-read-private user-read-email user-top-read"
+    
+    client = OAuth2Session(
+        client_id,
+        client_secret,
+        scope = scope,
+        redirect_uri="http://127.0.0.1:5000/callback"
+    )
+
+    token_endpoint = "https://accounts.spotify.com/api/token"
+
+    token = client.fetch_token(
+        token_endpoint,
+        authorization_response=request.url
+    )
+
+    session["token"] = token
+    return redirect("/results")
+
+# generating the playlist
+@app.route("/results")
+def results():
+    token = session.get("token")
+    if token is None:
+        return redirect("/login")
+
+    # geocode coords
+    coords = geocode_location(session["location"])
+    if coords is None:
+        return "Location not found."
+    
+    lat, lon = coords
+
+    # getting weather
+    weather = get_weather(lat, lon, session["date"])
+    if weather is None:
+        return "Weather data is unavailable."
+
+    # mapping the result to a genre
+    genre = map_weather_to_genre(weather)
+
+    # getting tracks based on that genre
+    length = int(session["length"])
+
+    tracks = get_spotify_tracks(genre, length, token)
+
+    return render_template("results.html",
+                           genre=genre,
+                           tracks=tracks,
+                           weather=weather)
+
+# running the app
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ functions.py
@@ -0,0 +1,144 @@
+import base64
+import urllib.parse, urllib.request, urllib.error, json
+import pprint
+import requests
+from authlib.integrations.requests_client import OAuth2Session
+from keys import client_id, client_secret, weather_api_key
+from datetime import datetime
+
+
+# getting the latitude and longitude of the place chosen because it is required for Open Weather
+def geocode_location(location):
+    baseurl = "http://api.openweathermap.org/geo/1.0/direct"
+
+    params = {
+        "q": location,
+        "limit": 1,
+        "appid": weather_api_key
+    }
+
+    fullurl = baseurl + "?" + urllib.parse.urlencode(params)
+
+    try:
+        resp = requests.get(fullurl)
+        resp.raise_for_status()
+        data = resp.json()
+
+        if len(data) == 0:
+            return None
+        
+        lat = data[0]["lat"]
+        lon = data[0]["lon"]
+
+        return lat, lon
+    
+    except Exception as e:
+        print("Error in geocoding: ", e)
+        return None
+
+# figuring out how many days in advance the user wants the playlist for
+# this will be needed for the free version of the weather api since it gives an 8 day forecase
+def days_ahead(selected_date):
+    today = datetime.now().date()
+    target = datetime.strptime(selected_date, "%Y-%m-%d").date()
+    return (target-today).days
+
+# getting the weather for that date and location
+def get_weather(lat, lon, selected_date):
+
+    baseurl = "https://api.openweathermap.org/data/3.0/onecall"
+
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": weather_api_key,
+        "units": "imperial"
+    }
+
+    fullurl = baseurl + "?" + urllib.parse.urlencode(params)
+
+    try:
+        resp = requests.get(fullurl)
+        resp.raise_for_status()
+        data = resp.json()
+
+        # how many days ahead, using previous function
+        offset = days_ahead(selected_date)
+
+        # use current day data if its for the same day
+        if offset == 0:
+            weather_data = data["current"]["weather"][0]["main"]
+            return weather_data
+
+        # otherwise use the daily forecast
+        else:
+            weather_data = data["daily"][offset]["weather"][0]["main"]
+            return weather_data
+    
+    except Exception as e:
+        print("Error retrieving weather: ", e)
+        return None
+
+
+# creating predefined logic to map each weather to a genre
+def map_weather_to_genre(weather_data):
+    # logic
+
+    weather_data = weather_data.strip()
+    if weather_data in ("Thunderstorm", "Drizzle", "Rain"):
+        genre = "chill"
+    
+    elif weather_data == "Snow":
+        genre = "chill"
+
+    elif weather_data in ("Mist", "Smoke", "Haze", "Dust", "Fog", "Sand", "Ash"):
+        genre = "chill"
+
+    elif weather_data in ("Squall", "Tornado"):
+        genre = "chill"
+
+    elif weather_data == "Clear":
+        genre = "chill"
+    
+    elif weather_data == "Clouds":
+        genre = "chill"
+    
+    else:
+        genre = "chill"
+
+    genre = "pop"
+
+    return genre
+
+def get_spotify_tracks(genre, limit, token):
+
+    # calls Spotify API
+    url = "https://api.spotify.com/v1/recommendations"
+
+    params = {
+        "seed_genres": genre,
+        "limit": limit
+    }
+
+    headers = {"Authorization": f"Bearer {token['access_token']}"}
+
+
+
+    resp = requests.get(url, headers=headers, params=params)
+    resp.raise_for_status()
+    data = resp.json()
+
+    if "tracks" in data:
+        tracklist = data["tracks"]
+    else:
+        tracklist = []
+
+    tracks = []
+    for item in tracklist:
+        tracks.append({
+            "name": item["name"],
+            "artist": item["artists"][0]["name"],
+            "url": item["external_urls"]["spotify"]
+        })
+
+    return tracks
